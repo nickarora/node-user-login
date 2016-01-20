5c1f2edf6df04419453a3077242744a097e563e0
Nick Arora <arora.nick@gmail.com>
2016-01-20 15:53:08 -0800

    update jade views and import bootstrap css framework

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // Handle file uploads
-//app.use(multer({dest: './uploads'}));
+// app.use(multer({dest: './uploads'}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
