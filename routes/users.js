aec649b4c834477e4af54da652cf3d37b5a041ca
Nick Arora <arora.nick@gmail.com>
2016-01-21 11:44:37 -0800

    show flash messages and style them

--- routes/users.js
+++ routes/users.js
@@ -148,4 +148,13 @@ router.post('/login',
     }
 );
 
+
+// GET Request for logout
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    req.flash('success', 'You have logged out.');
+    res.redirect('/');
+})
+
 module.exports = router;
