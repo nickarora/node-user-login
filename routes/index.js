ec0e8a497d9984a321755d19d6033e7169499a3c
Nick Arora <arora.nick@gmail.com>
2016-01-21 11:48:02 -0800

    redirect if not authenticated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET Members page. */
+router.get('/', ensureAuthentication, function(req, res, next) {
   res.render('index', { title: 'Members' });
 });
+
+function ensureAuthentication(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  }
+
+  res.redirect('/users/login');
+}
 
 module.exports = router;
